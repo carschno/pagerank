08fe694bef5d49148d6b0b346fa17231e6f57e8f
Carsten Schnober <c.schnober@jpberlin.de>
2015-03-05 12:21:56 +0100

    Input file location made slightly more flexible.

--- src/main/scala/com/schnobosoft/pagerank/WebGoogle.scala
+++ src/main/scala/com/schnobosoft/pagerank/WebGoogle.scala
@@ -2,11 +2,13 @@ package com.schnobosoft.pagerank
 
 /**
  * Run PageRank on data from {@link https://snap.stanford.edu/data/web-Google.html}.
+ * Adapt the location of the input file as appropriate.
  */
 object WebGoogle extends App {
   //  val nLines = Int.MaxValue // number of lines to read (in file: 5105043)
   val nPages = 916428
-  val location = "/home/schnober/corpora/web-Google.txt"
+
+  val location = System.getenv("HOME") + "/corpora/web-Google.txt"
 
   val r = PageRank.pagerank(location, nPages, method = PageRank.Method.MATRIX)
 
