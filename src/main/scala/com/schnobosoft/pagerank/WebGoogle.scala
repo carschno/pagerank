b84108d0fd9d8c3d43ecfec090d90c86f6f4288c
Carsten Schnober <c.schnober@jpberlin.de>
2015-03-13 11:26:49 +0100

    Result output changed.

--- src/main/scala/com/schnobosoft/pagerank/WebGoogle.scala
+++ src/main/scala/com/schnobosoft/pagerank/WebGoogle.scala
@@ -1,16 +1,20 @@
 package com.schnobosoft.pagerank
 
+import java.util.Locale
+
 /**
  * Run PageRank on data from {@link https://snap.stanford.edu/data/web-Google.html}.
  * Adapt the location of the input file as appropriate.
  */
 object WebGoogle extends App {
+  val outputNodes = List[Int](0, 99);
   //  val nLines = Int.MaxValue // number of lines to read (in file: 5105043)
   val nPages = 916428
 
   val location = System.getenv("HOME") + "/corpora/web-Google.txt"
 
-  val r = PageRank.pagerank(location, nPages, method = PageRank.Method.MATRIX)
+  val r = PageRank.pagerank(location, nPages, method = PageRank.Method.ITERATIVE, beta = 0.8)
 
-  println(r.slice(0, 10))
+  for (node <- outputNodes)
+    printf("Node %d:\t%E%n", node, r(node))
 }
